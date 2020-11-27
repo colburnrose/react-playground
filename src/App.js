686c04de57db461eff81c7ebae9f417db0d5df07
colburncodes <colburnsanders@gmail.com>
2020-11-26 21:34:57 -0800

    default props and components

--- src/App.js
+++ src/App.js
@@ -2,19 +2,32 @@ import "./App.css";
 
 import React from "react";
 import Split from "./composition/Split";
+import Tooltip from "./composition/Tooltip";
 
+const firstTooltip = (
+  <Tooltip color="hotpink" message="tooltip message">
+    ipsum
+  </Tooltip>
+);
+
+const secondTooltip = (
+  <Tooltip color="#126BCC" message="another tooltip message">
+    officiis
+  </Tooltip>
+);
 function App() {
   return (
     <main className="App">
       <Split className="left" flexBasis={3}>
-        This is the content for the left `Split`. Lorem ipsum dolor sit amet
-        consectetur, adipisicing elit. Incidunt ex velit suscipit facere
+        This is the content for the left `Split`. Lorem {firstTooltip} dolor sit
+        amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere
         officia?
+        <Tooltip message="one more tooltip message">Necessitatibus?</Tooltip>
       </Split>
       <Split className="right">
         This is the content for the right `Split`. Inventore aliquid cupiditate
-        suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus
-        id soluta aspernatur.
+        suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo
+        possimus id soluta aspernatur.
       </Split>
     </main>
   );
