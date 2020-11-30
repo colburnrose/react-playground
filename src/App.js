7e2c316dec9738d0f5894cac5ef093c696c89737
colburncodes <colburnsanders@gmail.com>
2020-11-29 19:49:28 -0800

    testing components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 
-import React from "react";
+import React, { Component } from "react";
 import Split from "./composition/Split";
 import Tooltip from "./composition/Tooltip";
+import Messages from "./Messages.js";
 
 const firstTooltip = (
   <Tooltip color="hotpink" message="tooltip message">
@@ -15,22 +16,34 @@ const secondTooltip = (
     officiis
   </Tooltip>
 );
-function App() {
-  return (
-    <main className="App">
-      <Split className="left" flexBasis={3}>
-        This is the content for the left `Split`. Lorem {firstTooltip} dolor sit
-        amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere
-        officia?
-        <Tooltip message="one more tooltip message">Necessitatibus?</Tooltip>
-      </Split>
-      <Split className="right">
-        This is the content for the right `Split`. Inventore aliquid cupiditate
-        suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo
-        possimus id soluta aspernatur.
-      </Split>
-    </main>
-  );
+// function App() {
+//   return (
+//     <main className="App">
+//       <Split className="left" flexBasis={3}>
+//         This is the content for the left `Split`. Lorem {firstTooltip} dolor sit
+//         amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere
+//         officia?
+//         <Tooltip message="one more tooltip message">Necessitatibus?</Tooltip>
+//       </Split>
+//       <Split className="right">
+//         This is the content for the right `Split`. Inventore aliquid cupiditate
+//         suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo
+//         possimus id soluta aspernatur.
+//       </Split>
+//     </main>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>APPLICATION NAME</h1>
+        <Messages name="Messages" unread={0} />
+        <Messages name="Notifications" unread={10} />
+      </div>
+    );
+  }
 }
 
 export default App;
