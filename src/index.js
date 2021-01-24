2e01d4e0d30564bec4da8976e9bec518e784b77f
colburncodes <colburnsanders@gmail.com>
2021-01-24 13:56:41 -0800

    error boundary

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./paltrows-power-toes/App";
-import AppLang from "./lang-context/AppLang";
-import RegistrationForm from "./registrationForm/registrationForm";
+import App from "./playingwithdanger/App";
 
 ReactDOM.render(
   <BrowserRouter>
-    {/* <App /> */}
-    {/* <AppLang /> */}
-    <RegistrationForm />
+    <App />
   </BrowserRouter>,
   document.getElementById("root")
 );
